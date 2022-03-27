bcc55f56577ee8d62d2abaf4c7ef6aa86211c7b2
Grigoriev Valerii <g.valera15102003@gmail.com>
2022-03-27 14:55:15 +0500

    cors fix

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,4 +1,4 @@
-function cors(req, res) {
+function cors(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
